19d3471b021ae204328f0559d33537e0a70351ab
Sam Hastings <samhastingsis@gmail.com>
2024-03-28 16:50:33 -0400

    118. Pascal's Triangle

--- Leetcode.playground/Pages/118. Pascal's Triangle.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/118. Pascal's Triangle.xcplaygroundpage/Contents.swift
@@ -1,1 +1,22 @@
+class Solution {
+    func generate(_ numRows: Int) -> [[Int]] {
+        var result: [[Int]] = [[]]
+        
+        result[0] = [1]
+        
+        for i in 1..<numRows {
+            var newRow = [1]
+            var prevRow = result[i-1]
+            for j in 1..<prevRow.count {
+                newRow.append(prevRow[j] + prevRow[j-1])
+            }
+            newRow.append(1)
+            result.append(newRow)
+        }
+        return result
+    }
+}
 
+let solution = Solution()
+
+print(solution.generate(5))
