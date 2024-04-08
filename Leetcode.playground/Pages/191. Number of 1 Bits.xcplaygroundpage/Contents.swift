5f294c718eaf9976aae2d0ba2ffd23859f577a65
Sam Hastings <samhastingsis@gmail.com>
2024-04-08 17:59:07 -0400

    202. Happy Hour

--- Leetcode.playground/Pages/191. Number of 1 Bits.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/191. Number of 1 Bits.xcplaygroundpage/Contents.swift
@@ -1,1 +1,13 @@
-print(2)
+class Solution {
+    func hammingWeight(_ n: Int) -> Int {
+        var n = n
+        var result = 0
+        for _ in 0..<32 {
+            result += n & 1
+            n = n >> 1
+        }
+        return result
+    }
+}
+
+print(Solution().hammingWeight(2147483645))
