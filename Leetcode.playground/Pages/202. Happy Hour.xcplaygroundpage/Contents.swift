05767006ce4b3c4c211fba2cb3419fc910e3b9fc
Sam Hastings <samhastingsis@gmail.com>
2024-04-09 16:27:31 -0400

    206. Reverse Linked List

--- Leetcode.playground/Pages/202. Happy Hour.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/202. Happy Hour.xcplaygroundpage/Contents.swift
@@ -1,1 +1,20 @@
-print(2)
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+        var result = n
+        var prevResults = Set<Int>()
+        while !prevResults.contains(result) {
+            prevResults.insert(result)
+            var new = 0
+            while result > 0 {
+                new += (result % 10) * (result % 10)
+                result /= 10
+            }
+            if new == 1 {return true}
+            result = new
+        }
+        return false
+    }
+}
+
+
+print(Solution().isHappy(2))
