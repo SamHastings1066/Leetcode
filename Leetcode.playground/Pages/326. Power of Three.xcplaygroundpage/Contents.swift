99e0e41dc91113051adc92075f986816c02fbd6e
Sam Hastings <samhastingsis@gmail.com>
2024-04-11 12:16:15 -0400

    326. Power of Three

--- Leetcode.playground/Pages/326. Power of Three.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/326. Power of Three.xcplaygroundpage/Contents.swift
@@ -1,1 +1,27 @@
-print(2)
+// Loop
+//class Solution {
+//    func isPowerOfThree(_ n: Int) -> Bool {
+//        if n == 1 {return true}
+//        var n = n
+//        while n > 0 {
+//            if n % 3 != 0 {return false}
+//            n /= 3
+//            if n == 1 { return true }
+//        }
+//        return false
+//    }
+//}
+
+// Recursion
+class Solution {
+    func isPowerOfThree(_ n: Int) -> Bool {
+        var n = n
+        if n == 1 {return true}
+        if n == 0 {return false}
+        if n % 3 != 0 {return false}
+        n /= 3
+        return isPowerOfThree(n)
+    }
+}
+
+print(Solution().isPowerOfThree(26))
