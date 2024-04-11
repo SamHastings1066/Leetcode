ad34cac747b9f436b98e53a666f0a078d0640b6b
Sam Hastings <samhastingsis@gmail.com>
2024-04-11 10:13:58 -0400

    242. Valid Anagram

--- Leetcode.playground/Pages/242. Valid Anagram.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/242. Valid Anagram.xcplaygroundpage/Contents.swift
@@ -1,1 +1,25 @@
-print(2)
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        guard s.count == t.count else {return false}
+        var s = Array(s.lowercased())
+        var t = Array(t.lowercased())
+        var sCount = [Character:Int]()
+        for letter in s {
+            sCount[letter, default: 0] += 1
+        }
+        for letter in t {
+            if let count = sCount[letter], count > 0 {
+                sCount[letter, default: 0] -= 1
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
+var s = "abba"
+var t = "baab"
+
+print(Solution().isAnagram(s, t))
