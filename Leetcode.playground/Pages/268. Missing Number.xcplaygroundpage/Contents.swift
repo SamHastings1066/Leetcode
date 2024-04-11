dae6d2092e24b85aa73d904679ed4c6f98a84bdb
Sam Hastings <samhastingsis@gmail.com>
2024-04-11 10:37:42 -0400

    268. Missing Number

--- Leetcode.playground/Pages/268. Missing Number.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/268. Missing Number.xcplaygroundpage/Contents.swift
@@ -1,1 +1,9 @@
-print(2)
+class Solution {
+    func missingNumber(_ nums: [Int]) -> Int {
+        var n = nums.count
+        return (n*(n+1))/2 - nums.reduce(0, +)
+    }
+}
+
+var nums = [9,6,4,2,3,5,7,0,1]
+print(Solution().missingNumber(nums))
