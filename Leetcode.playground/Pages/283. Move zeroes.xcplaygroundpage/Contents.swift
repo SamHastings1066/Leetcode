56b5cfe6e35b2572fa050cdd755f79e41c5e4a3b
Sam Hastings <samhastingsis@gmail.com>
2024-04-11 10:56:48 -0400

    283. Move zeroes

--- Leetcode.playground/Pages/283. Move zeroes.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/283. Move zeroes.xcplaygroundpage/Contents.swift
@@ -1,1 +1,21 @@
-print(2)
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var i = 0
+        var reps = 0
+        
+        while reps < nums.count {
+            if nums[i] == 0 {
+                nums.remove(at: i)
+                nums.append(0)
+            } else {
+                i += 1
+            }
+            reps += 1
+        }
+    }
+}
+
+
+var nums = [0,0,1]
+Solution().moveZeroes(&nums)
+print(nums)
