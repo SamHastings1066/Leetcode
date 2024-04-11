ebf7dfd7c07d660a687bfaedf6a56ce35d55a58b
Sam Hastings <samhastingsis@gmail.com>
2024-04-11 19:10:41 -0400

    387. First Unique Character in a String

--- Leetcode.playground/Pages/387. First Unique Character in a String.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/387. First Unique Character in a String.xcplaygroundpage/Contents.swift
@@ -1,1 +1,16 @@
-print(2)
+class Solution {
+    func firstUniqChar(_ s: String) -> Int {
+        var s = Array(s.lowercased())
+        var counts = [Character: Int]()
+        for char in s {
+            counts[char, default: 0] += 1
+        }
+        for i in s.indices {
+            if counts[s[i]] == 1 { return i }
+        }
+        return -1
+    }
+}
+
+var s = "aabb"
+print(Solution().firstUniqChar(s))
