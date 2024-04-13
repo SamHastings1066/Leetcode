6c0527c46a74ee4e8f08a6aa4aba7583fe6038d9
Sam Hastings <samhastingsis@gmail.com>
2024-04-13 10:26:55 -0400

    2. Add Two Numbers

--- LeetcodeMedium.playground/Pages/2. Add Two Numbers.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/2. Add Two Numbers.xcplaygroundpage/Contents.swift
@@ -1,1 +1,60 @@
-print(2)
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+func createLinkedList(from array: [Int]) -> ListNode? {
+    guard !array.isEmpty else {return nil}
+    let head = ListNode(array.first!)
+    var current = head
+    for value in array.dropFirst() {
+        var newNode = ListNode(value)
+        current.next = newNode
+        current = newNode
+    }
+    return head
+}
+
+func printNodeValues(_ head: ListNode?) {
+    guard head != nil else {print("Empty list"); return}
+    var node = head
+    while let currentNode = node {
+        print(currentNode.val)
+        node = currentNode.next
+    }
+}
+
+// My solution
+class Solution {
+    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        var dummyHead = ListNode()
+        var current = dummyHead
+        var l1 = l1
+        var l2 = l2
+        var carriedOne = 0
+        while l1 != nil || l2 != nil || carriedOne != 0 {
+            var val1 = l1?.val ?? 0
+            var val2 = l2?.val ?? 0
+            var sum = val1 + val2 + carriedOne
+            carriedOne = sum / 10
+            current.next = ListNode(sum % 10)
+            current = current.next!
+            l1 = l1?.next
+            l2 = l2?.next
+        }
+        return dummyHead.next
+    }
+}
+
+var l1 = [2,4,3]
+var l2 = [5,6,4]
+//var l1 = [9,9,9,9,9,9,9]
+//var l2 = [9,9,9,9]
+
+var l1Head = createLinkedList(from: l1)
+var l2Head = createLinkedList(from: l2)
+
+printNodeValues(Solution().addTwoNumbers(l1Head, l2Head))
