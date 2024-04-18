e5bedad7e7ab1ab878619fc2c31e51ded0d796d0
Sam Hastings <samhastingsis@gmail.com>
2024-04-18 16:41:06 -0400

    7. Reverse Integer

--- LeetcodeMedium.playground/Pages/7. Reverse Integer.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/7. Reverse Integer.xcplaygroundpage/Contents.swift
@@ -1,1 +1,27 @@
-print(2)
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        
+        var x = x
+        var ans: Int = 0
+
+        while x != 0 {
+            let pop = x % 10
+            
+            if ans > Int.max || (ans == Int.max && pop > 7) {
+                return 0
+            }
+            if ans < Int.min || (ans == Int.min && pop < -8) {
+                return 0
+            }
+            
+            
+            x /= 10
+            ans = 10 * ans + pop
+        }
+        return ans
+    }
+}
+
+
+
+print(Solution().reverse(-120))
