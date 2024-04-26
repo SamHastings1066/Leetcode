a35e9aa59cc2547572bce4a8d7714830ecbcf7cc
Sam Hastings <samhastingsis@gmail.com>
2024-04-26 17:04:29 -0400

    22. Generate Parentheses

--- LeetcodeMedium.playground/Pages/22. Generate Parentheses.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/22. Generate Parentheses.xcplaygroundpage/Contents.swift
@@ -1,5 +1,35 @@
 class Solution {
     func generateParenthesis(_ n: Int) -> [String] {
+        var ans = [String]()
+        var currString = ""
+        backtracking(0, 0)
+        return ans
+        
+        func backtracking(_ leftCount: Int, _ rightCount: Int) {
+            
+            if currString.count == 2*n {
+                ans.append(currString)
+                return
+            }
+            
+            if leftCount < n {
+                currString.append("(")
+                backtracking(leftCount + 1, rightCount)
+                currString.removeLast()
+            }
+            
+            if rightCount < leftCount {
+                currString.append(")")
+                backtracking(leftCount, rightCount + 1)
+                currString.removeLast()
+            }
+            
+        }
         
     }
 }
+
+print(Solution().generateParenthesis(4))
+
+
+
