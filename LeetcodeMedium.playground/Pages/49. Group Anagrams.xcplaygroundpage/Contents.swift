61d9db6d81709d368d3837a9b21e616495f4d5df
Sam Hastings <samhastingsis@gmail.com>
2024-05-04 12:38:29 -0400

    49. Group Anagrams

--- LeetcodeMedium.playground/Pages/49. Group Anagrams.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/49. Group Anagrams.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
+//Solution using sorting
+//class Solution {
+//    func groupAnagrams(_ strs: [String]) -> [[String]] {
+//        var strs = strs
+//        var ans = [String:[String]]()
+//        for s in strs {
+//            ans[String(s.sorted()), default: []].append(s)
+//        }
+//        
+//        return ans.values.map{$0}
+//    }
+//}
 
-import Foundation
+// Using count of asciiValues
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var ans = [[Int]:[String]]()
+        for s in strs {
+            ans[charMap(s), default: []].append(s)
+        }
+        
+        return ans.values.map{$0}
+    }
+    func charMap(_ s: String) -> [Int] {
+        var ans = Array(repeating: 0, count: 26)
+        for char in s {
+            ans[Int(char.asciiValue!) - 97] += 1
+        }
+        return ans
+    }
+}
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+var strs = ["eat","tea","tan","ate","nat","bat"]
+
+
+print(Solution().groupAnagrams(strs))
