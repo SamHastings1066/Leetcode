a78e84e6dc058a71e71690ed1e56b75d401f435c
Sam Hastings <samhastingsis@gmail.com>
2024-05-17 09:26:47 -0400

    71. Simplify path completed

--- LeetcodeMedium.playground/Pages/71. Simplify Path.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/71. Simplify Path.xcplaygroundpage/Contents.swift
@@ -1,8 +1,23 @@
-//: [Previous](@previous)
+class Solution {
+    func simplifyPath(_ path: String) -> String {
+        var arr = path.split(separator: "/")
+        var res = [String]()
+        for ele in arr {
+            if ele == ".." {
+                if res.count > 0 {
+                    res.removeLast()
+                }
+            } else if ele == "." {
+                continue
+            } else {
+                res.append(String(ele))
+            }
+        }
+        return "/" + res.joined(separator: "/")
+        
+    }
+}
 
-import Foundation
+var path = "/a/./b/../../c/"
 
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+print(Solution().simplifyPath(path))
