c3decfca18f9fa714c28d61d6a9af3dda1bdf682
Sam Hastings <samhastingsis@gmail.com>
2024-05-19 11:43:27 -0400

    77. Combinations

---
+++ LeetcodeMedium.playground/Pages/77. Combinations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+// My approach
+//class Solution {
+//    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+//        
+//        var res = [[Int]]()
+//        
+//        func backtrack(_ arr: [Int], _ path: [Int]) {
+//            var path = path
+//            var arr = arr
+//            if path.count == k {
+//                res.append(path)
+//                return
+//            }
+//            
+//            while arr.count>0 {
+//                path.append(arr.removeFirst())
+//                backtrack(arr, path)
+//                path.removeLast()
+//            }
+//        }
+//
+//        backtrack(Array(1...n), [])
+//        
+//        return res
+//        
+//    }
+//}
+
+// Model answer
+class Solution {
+    func combine(_ n: Int, _ k: Int) -> [[Int]] {
+        
+        var res = [[Int]]()
+        
+        func backtrack(_ firstNum: Int, _ path: [Int]) {
+            var path = path
+            if path.count == k {
+                res.append(path)
+                return
+            }
+            
+            var need = k - path.count
+            var remain = n - firstNum + 1
+            var available = remain - need
+            
+            for i in firstNum..<firstNum+available+1 {
+                path.append(i)
+                
+                backtrack(i+1, path)
+                path.removeLast()
+            }
+        }
+        
+        backtrack(1, [])
+        
+        return res
+        
+    }
+}
+
+print(Solution().combine(13, 13))
