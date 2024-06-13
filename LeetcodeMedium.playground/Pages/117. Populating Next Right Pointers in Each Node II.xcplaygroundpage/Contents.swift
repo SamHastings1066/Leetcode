b4064be0d1ac159cb9175a88a8a1578d1686e1f6
Sam Hastings <samhastingsis@gmail.com>
2024-06-13 17:55:51 -0400

    117. Populating Next Right Pointers in Each Node II

--- LeetcodeMedium.playground/Pages/117. Populating Next Right Pointers in Each Node II.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/117. Populating Next Right Pointers in Each Node II.xcplaygroundpage/Contents.swift
@@ -1,7 +1,102 @@
-//: [Previous](@previous)
+public class Node {
+    public var val: Int
+    public var left: Node?
+    public var right: Node?
+      public var next: Node?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+        self.next = nil
+    }
+}
 
-import Foundation
+// First attempt: The idea is to to a BFS; keep track of the currentLevelLength and the nextLevelLength. When current level length = 0, then you are at the end of the level so the next pointer sould be nil, otherwise, if there are any nodes left in the queue, the next pointer should point to the next node in the queue, and if there aren't any nodes left in the queue then you are at the end of the BFS so the next pointer for the current node should also be nil.
+//class Solution {
+//    func connect(_ root: Node?) -> Node? {
+//        var currentLevelLength = 1
+//        var nextLevelLength = 0
+//        var queue = [root]
+//        while !queue.isEmpty {
+//            var curr = queue.removeFirst()
+//            currentLevelLength -= 1
+//            if let left = curr?.left {
+//                queue.append(left)
+//                nextLevelLength += 1
+//            }
+//            if let right = curr?.right {
+//                queue.append(right)
+//                nextLevelLength += 1
+//            }
+//            if currentLevelLength == 0 {
+//                currentLevelLength = nextLevelLength
+//                nextLevelLength = 0
+//                curr?.next = nil
+//            } else if let next = queue.first {
+//                curr?.next = next
+//            } else {
+//                curr?.next = nil
+//            }
+//        }
+//        return root
+//    }
+//}
 
-var greeting = "Hello, playground"
+// Pretty much the same, just using queue.count and an internal for loop
+//class Solution {
+//    func connect(_ root: Node?) -> Node? {
+//        var queue = [root]
+//        while !queue.isEmpty {
+//            let size = queue.count
+//            
+//            for i in 0..<size {
+//                var curr = queue.removeFirst()
+//                if i < size - 1 {
+//                    curr?.next = queue.first!
+//                }
+//                if let left = curr?.left {
+//                    queue.append(left)
+//                }
+//                if let right = curr?.right {
+//                    queue.append(right)
+//                }
+//            }
+//            
+//        }
+//        return root
+//    }
+//}
 
-//: [Next](@next)
+
+// Linked list
+class Solution {
+    
+    func process(_ child: Node?, _ prev: Node?, _ leftmost: Node?) -> (Node?, Node?) {
+        var leftmost = leftmost, prev = prev
+        if child != nil {
+            if let prev {
+                prev.next = child
+            } else {
+                leftmost = child
+            }
+            prev = child
+        }
+        return (prev, leftmost)
+    }
+    
+    func connect(_ root: Node?) -> Node? {
+        var leftmost = root
+        while leftmost != nil {
+            var curr = leftmost
+            var prev: Node? = nil
+            leftmost = nil
+            while curr != nil {
+                (prev, leftmost) = process(curr?.left, prev, leftmost)
+                (prev, leftmost) = process(curr?.right, prev, leftmost)
+                curr = curr?.next
+            }
+            
+        }
+        return root
+    }
+}
