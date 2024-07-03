49a265fbd53e84af90c4ef53028ef49c0f3e5726
Sam Hastings <samhastingsis@gmail.com>
2024-07-03 10:16:49 -0400

    45. Jump Game II

--- LeetcodeMedium.playground/Pages/45. Jump Game II.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/45. Jump Game II.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
-//: [Previous](@previous)
+// First attempt
+//class Solution {
+//    func jump(_ nums: [Int]) -> Int {
+//        var n = nums.count
+//        var moves = [Int]()
+//        func minJump(_ move: Int, _ pos: Int) {
+//            print(pos, n)
+//            if pos >= n-1 {
+//                moves.append(move)
+//                return
+//            }
+//            for i in 0..<nums[pos] {
+//                if pos + i + 1 < n {
+//                    minJump(move + 1, pos + i + 1)
+//                }
+//            }
+//        }
+//        minJump(0, 0)
+//        return moves.min() ?? 0
+//    }
+//}
 
-import Foundation
+// Greedy
+class Solution {
+    func jump(_ nums: [Int]) -> Int {
+        var jumps = 0, end = 0, far = 0, n = nums.count
+        for i in 0..<n-1 {
+            far = max(far, nums[i] + i)
+            if i == end {
+                jumps += 1
+                end = far
+            }
+        }
+        return jumps
+    }
+}
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+var nums = [2,3,1,1,4]
+print(Solution().jump(nums))
