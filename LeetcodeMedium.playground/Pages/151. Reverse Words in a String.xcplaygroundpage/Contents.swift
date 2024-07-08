6e98694075e2417d27242ed3aa754432bc60b458
Sam Hastings <samhastingsis@gmail.com>
2024-07-08 16:23:58 -0400

    151. Reverse Words in a String

--- LeetcodeMedium.playground/Pages/151. Reverse Words in a String.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/151. Reverse Words in a String.xcplaygroundpage/Contents.swift
@@ -1,7 +1,63 @@
-//: [Previous](@previous)
+// First approach
+//class Solution {
+//    func reverseWords(_ s: String) -> String {
+//        var s = Array(s)
+//        var ans: [Character] = []
+//        var word: [Character] = []
+//        for char in s {
+//            if char == " " {
+//                if !word.isEmpty {
+//                    word.append(" ")
+//                    ans.insert(contentsOf: word, at: 0)
+//                    word = []
+//                }
+//            } else {
+//                word.append(char)
+//            }
+//        }
+//        word.append(" ")
+//        ans.insert(contentsOf: word, at: 0)
+//        ans.removeLast()
+//        if let space = ans.first {
+//            if space == " " { ans.removeFirst()}
+//        }
+//        return String(ans)
+//    }
+//}
 
-import Foundation
+// Cleaner implementation of above:
+class Solution {
+    func reverseWords(_ s: String) -> String {
+        var ans: [String] = []
+        var word: String = ""
+        
+        for char in s {
+            if char == " " {
+                if !word.isEmpty {
+                    ans.insert(word, at: 0)
+                    word = ""
+                }
+            } else {
+                word.append(char)
+            }
+        }
+        
+        if !word.isEmpty {
+            ans.insert(word, at: 0)
+        }
+        
+        return ans.joined(separator: " ")
+    }
+}
 
-var greeting = "Hello, playground"
+// Swift in-built methods
+//class Solution {
+//    func reverseWords(_ s: String) -> String {
+//        return s.split(separator: " ").reversed().joined(separator: " ")
+//    }
+//}
 
-//: [Next](@next)
+
+let s = "the sky is blue"
+
+print (Solution().reverseWords(s))
