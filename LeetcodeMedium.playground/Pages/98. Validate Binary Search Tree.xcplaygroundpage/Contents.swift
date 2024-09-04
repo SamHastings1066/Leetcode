b288c04decbf33ccea489e969819b691fa79b1e8
Sam Hastings <samhastingsis@gmail.com>
2024-09-04 18:24:25 -0400

    98. Validate Binary Search Tree

---
+++ LeetcodeMedium.playground/Pages/98. Validate Binary Search Tree.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+// My first solution
+//class Solution {
+//    func isValidBST(_ root: TreeNode?) -> Bool {
+//        var ans = true
+//        check(root, nil, nil)
+//        return ans
+//        
+//        func check(_ node: TreeNode?, _ low: Int?, _ high: Int?) {
+//            if node == nil { return }
+//            if let low {
+//                if node!.val <= low {
+//                    ans = false
+//                    return
+//                }
+//            }
+//            if let high {
+//                if node!.val >= high {
+//                    ans = false
+//                    return
+//                }
+//            }
+//            check(node?.left, low, node!.val)
+//            check(node?.right, node!.val, high)
+//        }
+//    }
+//}
+
+// Refactored solution
+//class Solution {
+//    func isValidBST(_ root: TreeNode?) -> Bool {
+//        return isBST(root, Int.min, Int.max)
+//        
+//        func isBST(_ node: TreeNode?, _ low: Int, _ high: Int) -> Bool {
+//            guard let node else { return true }
+//            guard node.val > low && node.val < high else { return false }
+//            return isBST(node.left, low, node.val) && isBST(node.right, node.val, high)
+//        }
+//    }
+//}
+
+// In order traversal
+class Solution {
+    
+    var prev: Int?
+    
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        return inorder(root)
+    }
+    
+    func inorder(_ node: TreeNode?) -> Bool {
+        guard let node else { return true }
+        guard inorder(node.left) else { return false }
+        if let prev, prev >= node.val { return false }
+        prev = node.val
+        return inorder(node.right)
+    }
+}
