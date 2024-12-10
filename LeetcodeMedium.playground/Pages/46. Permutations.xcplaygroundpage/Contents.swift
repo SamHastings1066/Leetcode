e974d1a9b55631bebb7df944938ed0ba1336281c
Sam Hastings <samhastingsis@gmail.com>
2024-12-10 09:42:29 -0500

    Use Set for constant time lookup.

--- LeetcodeMedium.playground/Pages/46. Permutations.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/46. Permutations.xcplaygroundpage/Contents.swift
@@ -23,28 +23,52 @@
 //    }
 //}
 
+// Using array - cotains takes O(n) time
+//class Solution {
+//    func permute(_ nums: [Int]) -> [[Int]] {
+//        var ans = [[Int]]()
+//        backtrack([])
+//        return ans
+//        
+//        func backtrack(_ path: [Int]) {
+//            var path = path
+//            if path.count == nums.count {
+//                ans.append(path)
+//                return
+//            }
+//            for num in nums {
+//                if !path.contains(num) {
+//                    path.append(num)
+//                    backtrack(path)
+//                    path.removeLast()
+//                }
+//            }
+//            
+//        }
+//    }
+//}
+
+// Using Set - .cotains takes O(1) time
 class Solution {
     func permute(_ nums: [Int]) -> [[Int]] {
         var ans = [[Int]]()
+        var used = Set<Int>() // To track used elements
         backtrack([])
         return ans
         
         func backtrack(_ path: [Int]) {
-            var path = path
             if path.count == nums.count {
                 ans.append(path)
                 return
             }
             for num in nums {
-                if !path.contains(num) {
-                    path.append(num)
-                    backtrack(path)
-                    path.removeLast()
+                if !used.contains(num) { // Constant-time check
+                    used.insert(num)     // Mark as used
+                    backtrack(path + [num]) // Append num to path
+                    used.remove(num)     // Backtrack: unmark num
                 }
             }
-            
         }
     }
 }
-
 print(Solution().permute([1,2,3]))
