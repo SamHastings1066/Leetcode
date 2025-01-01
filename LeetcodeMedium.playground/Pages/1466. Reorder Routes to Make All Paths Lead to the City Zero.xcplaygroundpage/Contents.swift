37614bb46f8a292eb2975deeb3ce1f22009ad8ac
Sam Hastings <samhastingsis@gmail.com>
2025-01-01 13:02:51 -0500

    Update 1466. Reorder Routes to Make All Paths Lead to the City Zero

--- LeetcodeMedium.playground/Pages/1466. Reorder Routes to Make All Paths Lead to the City Zero.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/1466. Reorder Routes to Make All Paths Lead to the City Zero.xcplaygroundpage/Contents.swift
@@ -1,4 +1,8 @@
 // My first solution: Time - O(n), Space - O(n)
+/*
+ Here, we have a directed graph given as an array of edges. What edges do we need to swap? The problem states that there is only one way to travel between two different cities. We want every city to be able to reach 0, and because there is only one road between cities, all roads must be directed towards 0. This means we can traverse away from 0, and anytime we see that an edge is pointing away from 0, we know we need to swap it.
+ */
+ 
 class Solution {
     func minReorder(_ n: Int, _ connections: [[Int]]) -> Int {
         var graph = [Int:[(Int,Bool)]]()
@@ -14,6 +18,7 @@ class Solution {
         dfs(0)
         return ans
 
+        // This dfs is traversing the tree in the opposite direction to that which we actually want to travel. Therefore, any time we use an edge in the original direction we know it is in the wrong direction and must increment ans by 1.
         func dfs(_ node: Int) {
             if seen.contains(node) { return }
             seen.insert(node)
