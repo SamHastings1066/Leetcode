0dc4cfcdbdeb29f2a4c5ba1e3a879620de59cccb
Sam Hastings <samhastingsis@gmail.com>
2025-01-01 13:25:51 -0500

    Add slicker implementation for 841. Keys and Rooms

--- LeetcodeMedium.playground/Pages/841. Keys and Rooms.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/841. Keys and Rooms.xcplaygroundpage/Contents.swift
@@ -1,16 +1,37 @@
+// First implementation
+//class Solution {
+//    func canVisitAllRooms(_ rooms: [[Int]]) -> Bool {
+//        var seen = Set<Int>()
+//        var visited = 0
+//        dfs(0)
+//        return visited == rooms.count
+//
+//        func dfs(_ room: Int) {
+//            if seen.contains(room) { return }
+//            seen.insert(room)
+//            visited += 1
+//            for neigh in rooms[room] {
+//                dfs(neigh)
+//            }
+//            
+//        }
+//    }
+//}
+
+// Slicker implementation - only do dfs if neigh not in seen; don't use visited, just check is seen is the same size as rooms,
 class Solution {
     func canVisitAllRooms(_ rooms: [[Int]]) -> Bool {
         var seen = Set<Int>()
-        var visited = 0
+        seen.insert(0)
         dfs(0)
-        return visited == rooms.count
+        return seen.count == rooms.count
 
         func dfs(_ room: Int) {
-            if seen.contains(room) { return }
             seen.insert(room)
-            visited += 1
             for neigh in rooms[room] {
-                dfs(neigh)
+                if !seen.contains(neigh) {
+                    dfs(neigh)
+                }
             }
             
         }
