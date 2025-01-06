6ed57269fbb47fcc57347250e26e224173fd4270
Sam Hastings <samhastingsis@gmail.com>
2025-01-06 11:54:48 -0500

    Update 487. Max Consecutive Ones II

--- LeetcodeMedium.playground/Pages/487. Max Consecutive Ones II.xcplaygroundpage/Contents.swift
+++ LeetcodeMedium.playground/Pages/487. Max Consecutive Ones II.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@ class Solution {
             if nums[right] == 0 {
                 zeroCount += 1
             }
-            while zeroCount > 1, left<nums.count {
+            while zeroCount > 1 {
                 left += 1
                 if nums[left-1] == 0 {
                     zeroCount -= 1
